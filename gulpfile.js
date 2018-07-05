8e139ab4d108b8dea5c1eca0c28f81468664de9e
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-07-05 11:24:41 +0200

    Add content

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,30 @@ const del = require("del");
 const exec = require("child_process").exec;
 
 
-gulp.task("clean", function () {
-    return del(["build"]);
-});
-
-gulp.task("copySources", ["clean"], function () {
-    return gulp.src(["src/**"])
-        .pipe(gulp.dest("build"));
-});
-
-gulp.task("build", ["copySources"], function (callback) {
-    exec("static-i18n -l en -i en -i de src -o build --fileFormat yaml", function (err, stdout, stderr) {
+function translate(callback) {
+    exec("static-i18n -l en -i en -i de -i nl src -o build --fileFormat yaml", function (err, stdout, stderr) {
         if (stdout) console.log(stdout);
         if (stderr) console.log(stderr);
         callback(err);
     })
+}
+
+gulp.task("clean", function () {
+    return del(["build"]);
 });
+
+gulp.task("copySources", function () {
+    return gulp.src(["src/**"])
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", ["clean", "copySources"], translate);
+
+gulp.task("buildWithoutClean", ["copySources"], translate);
 
 gulp.task("watch", ["clean"], function () {
     return gulpWatch(["src/**/*", "locales/**/*"], {ignoreInitial: false}, function () {
-        runSequence("build");
+        runSequence("buildWithoutClean");
     });
 });
 
